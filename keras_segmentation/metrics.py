619f04008066aa2b53bf41ef755403fbd34c0f2f
Gabriel Antonio <gabri14el@gmail.com>
2021-07-23 10:45:22 +0100

    for debug

--- keras_segmentation/metrics.py
+++ keras_segmentation/metrics.py
@@ -15,6 +15,26 @@ def get_iou(gt, pr, n_classes):
 
 #dice coefficient implementation
 def dice_coef(y_true, y_pred, smooth=1):
+    print('true: ', y_true.shape)
+    print('predict: ', y_pred)
     intersection = K.sum(y_true * y_pred, axis=[1,2,3])
     union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3])
     return K.mean( (2. * intersection + smooth) / (union + smooth), axis=0)
+
+#f1 score implementation
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
